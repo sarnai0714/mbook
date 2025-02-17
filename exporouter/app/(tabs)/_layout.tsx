343a5af4c0d46a53020d7677681f09fe96e3431c
sarnai0714 <sw22d033@mandakh.edu.mn>
2025-02-18 00:10:53 +0800

    about

--- exporouter/app/(tabs)/_layout.tsx
+++ exporouter/app/(tabs)/_layout.tsx
@@ -6,7 +6,7 @@ const _layout = () => {
     {/* <Tabs.Screen name="home"></Tabs.Screen> */}
      <Tabs.Screen name="index" options={{title:"Home", tabBarIcon: () => <AntDesign name="home" size={28} color="aqua" />,}}></Tabs.Screen>
      <Tabs.Screen name="user/[id]" options={{title:"User", tabBarIcon: () => <AntDesign size={28} name="user" color="aqua" />,}}></Tabs.Screen>
-     <Tabs.Screen name="About" options={{  title: "About",tabBarIcon: () => <AntDesign name="rightcircle" size={28} color="aqua" />, }}></Tabs.Screen>
+     <Tabs.Screen name="About" options={{  headerTitle: "About",tabBarIcon: () => <AntDesign name="rightcircle" size={28} color="aqua" />, }}></Tabs.Screen>
    </Tabs>
  );
 };
