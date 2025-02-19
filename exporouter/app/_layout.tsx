0bea06d4cb5568cec90048bb5ea0a3652c71d31a
sarnai0714 <sw22d033@mandakh.edu.mn>
2025-02-19 16:08:13 +0800

    ab

--- exporouter/app/_layout.tsx
+++ exporouter/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 const MainLayout = () => {
  return (
    <Stack>
-     <Stack.Screen name="(tabs)" options={{ headerTitle: "Home Page", headerShown:false }} />
+     <Stack.Screen name="(tabs)" options={{ headerTitle: "Home Page", headerShown:false, }} />
      <Stack.Screen name="Aboutus.tsx" options={{ headerTitle: "About us" }} />
 <Stack.Screen name="+not-found.tsx" />
    </Stack>
